f7ba0f7951377dfb9e39a99640c11e4a0297a66a
Yashmori09 <yashmori0901@gmail.com>
2024-01-07 14:21:28 +0530

    data transformation added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from textSummarizer.logging import logger
 
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationPipeline
-# from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationPipeline
 # from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 # from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textSummarizer.logging import logger
@@ -24,6 +24,18 @@ STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
---
+++ src/textSummarizer/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,17 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.components.data_transformation import DataTransformation
+from textSummarizer.logging import logger
+
+class DataTransformationPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.convert()
+
+
+if __name__ == "__main__":
+    data_validation_pipeline = DataTransformationPipeline()
+    data_validation_pipeline.main()
