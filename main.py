ccc3c50697296b8a34ee4ea5d08a84f00b13a69a
Yashmori09 <yashmori0901@gmail.com>
2024-01-07 13:30:15 +0530

    data validation done

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from textSummarizer.logging import logger
 
 
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionPipeline
-# from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationPipeline
 # from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 # from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 # from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
@@ -13,6 +13,17 @@ STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
---
+++ src/textSummarizer/pipeline/stage_02_data_validation.py
@@ -0,0 +1,17 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.components.data_validation import DataValiadtion
+from textSummarizer.logging import logger
+
+class DataValidationPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_files_exist()
+
+
+if __name__ == "__main__":
+    data_validation_pipeline = DataValidationPipeline()
+    data_validation_pipeline.main()
